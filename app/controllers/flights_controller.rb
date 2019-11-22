606f8b848e7f3752b23b84c8c09ce910d9e6dcd6
Raghu Betina <raghu@firstdraft.com>
2019-11-22 20:35:46 +0000

    Only flight user can modify flight

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,4 +1,14 @@
 class FlightsController < ApplicationController
+  before_action :current_user_must_be_flight_user, :only => [:edit_form, :update_row, :destroy_row]
+
+  def current_user_must_be_flight_user
+    flight = Flight.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
+
+    unless current_user == flight.user
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @flights = Flight.all
 
