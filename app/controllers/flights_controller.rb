7fc825c6c0a54256abe7368e595f7a98ebbc52a3
Raghu Betina <raghu@firstdraft.com>
2019-11-22 20:35:46 +0000

    Only flight user can access show page

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,5 +1,5 @@
 class FlightsController < ApplicationController
-  before_action :current_user_must_be_flight_user, :only => [:edit_form, :update_row, :destroy_row]
+  before_action :current_user_must_be_flight_user, :only => [:show, :edit_form, :update_row, :destroy_row]
 
   def current_user_must_be_flight_user
     flight = Flight.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
